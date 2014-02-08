f1599e3ae501eb0b85119649fb4a882257e3fdb5
Simon BOEUF <simon.boeuf@gmail.com>
2014-02-08 20:01:46 +0100

    refacto

--- server/buff_handler.c
+++ server/buff_handler.c
@@ -9,10 +9,23 @@ void	add_bit_to_char(char bit)
 	byte += bit;
 	if (++size == 8)
 	{
-		if (byte == 0)
-			ft_putendl("");
-		else
-			ft_putchar(byte);
+		add_char_to_string(byte);
 		size = 0;
+	}
+}
+
+void	add_char_to_string(char c)
+{
+	static t_clist	*string = NULL;
+
+	if (string == NULL)
+		string = clist_new(c);
+	else
+		clist_add(string, c);
+	if (c == 0)
+	{
+		clist_print(string);
+		clist_del(string);
+		string = NULL;
 	}
 }
--- server/includes/server.h
+++ server/includes/server.h
@@ -6,11 +6,23 @@
 # include <signal.h>
 # include "../../libft/includes/libft.h"
 
+typedef struct	s_clist
+{
+	char			c;
+	struct s_clist	*next;
+}				t_clist;
+
 void	display_pid(void);
 void	launch_server(void);
 
 void	catch(int s, siginfo_t *info, void *context);
 
 void	add_bit_to_char(char bit);
+void	add_char_to_string(char c);
+
+t_clist	*clist_new(char c);
+void	clist_add(t_clist *list, char c);
+void	clist_print(t_clist *list);
+void	clist_del(t_clist *list);
 
 #endif
